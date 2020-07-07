ebd644cab3b7a79decbb4349a8e8c7b84da157e0
Akshay Sharma <akshay2403sharma@gmail.com>
2020-07-07 14:59:02 +0530

    Remove unwanted console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('hello from public javascript file')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
